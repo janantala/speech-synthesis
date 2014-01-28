b650ba1c0221bba41ca8df54675c96cfdb0b574f
Jan Antala <jan@antala.sk>
2014-01-28 18:17:19 +0100

    feat(): add SpeechSynthesisUtterance Events

--- polyfill.js
+++ polyfill.js
@@ -2,29 +2,96 @@
   'use strict';
 
   var SpeechSynthesisUtterance = function(text){
+
+    /**
+     * SpeechSynthesisUtterance Attributes
+     */
     this.text = text || '';
     this.lang = document.documentElement.lang || 'en-US';
-    this.voiceURI = '';
     this.volume = 1.0; // 0 to 1
     this.rate = 1.0; // 0.1 to 10
-    this.pitch = 1.0; //0 to 2
+    // These attributes are not supported:
+    this.voiceURI = 'native';
+    this.pitch = 1.0; //0 to 2;
+
+    /**
+     * SpeechSynthesisUtterance Events
+     */
+    this.onstart = undefined;
+    this.onend = undefined;
+    this.onerror = undefined;
+    this.onpause = undefined;
+    this.onresume = undefined;
+    // These attributes are not supported:
+    this.onmark = undefined;
+    this.onboundary = undefined;
+
 
     this.corsProxyServer = 'http://www.corsproxy.com/';
 
+    /**
+     * Private parts
+     */
     var that = this;
+
+    var startTime;
+    var endTime;
+    var event = {
+      charIndex: undefined,
+      elapsedTime: undefined,
+      name: undefined
+    };
+
+    var updateElapsedTime = function(){
+      endTime = new Date().getTime();
+      event.elapsedTime = (endTime - (startTime || endTime)) / 1000;
+    };
+
     this._initAudio = function(){
       var audio = new Audio();
 
       audio.addEventListener('play', function() {
         console.log('play');
+        updateElapsedTime();
+
+        if (! startTime) {
+          startTime = new Date().getTime();
+          if (that.onstart) {
+            that.onstart(event);
+          }
+        }
+        else {
+          if (that.onresume) {
+            that.onresume(event);
+          }
+        }
       }, false);
 
       audio.addEventListener('ended', function() {
         console.log('ended');
+        updateElapsedTime();
+        
+        if (that.onend) {
+          that.onend(event);
+        }
       }, false);
 
       audio.addEventListener('error', function() {
         console.log('error');
+        updateElapsedTime();
+        
+        if (that.onerror) {
+          that.onerror(event);
+        }
+      }, false);
+
+      audio.addEventListener('pause', function() {
+        console.log('pause');
+        updateElapsedTime();
+        
+        if (that.onpause) {
+          that.onpause(event);
+        }
       }, false);
 
       var audioURL = [that.corsProxyServer, 'translate.google.com/translate_tts?ie=UTF-8&q=', that.text , '&tl=', that.lang].join('');
@@ -83,6 +150,10 @@
         console.log('SpeechSynthesis audio error');
         playNext(utteranceQueue);
       }, false);
+
+      audio.addEventListener('pause', function() {
+        console.log('SpeechSynthesis audio pause');
+      }, false);
     };
 
     var speak = function(SpeechSynthesisUtterance){
@@ -95,9 +166,7 @@
         // do nothing else
       }
       else {
-        that.speaking = true;
         playNext(utteranceQueue);
-        resume();
       }
     };
 
@@ -118,6 +187,7 @@
 
     var resume = function(){
       audio.play();
+      that.speaking = true;
       that.paused = false;
       console.log(that);
     };
@@ -168,8 +238,17 @@ u.lang = 'en-US';
 u.volume = 0.1; // 0 to 1
 u.rate = 0.5; // 0.1 to 10
 u.pitch = 2; //0 to 2
-u.onend = function(event) { alert('Finished in ' + event.elapsedTime + ' seconds.'); };
+u.onend = function(event) { console.log('Finished in ' + event.elapsedTime + ' seconds.'); };
+u.onpause = function(event) { console.log('Paused in ' + event.elapsedTime + ' seconds.'); };
+u.onresume = function(event) { console.log('Resumed in ' + event.elapsedTime + ' seconds.'); };
 speechSynthesis.speak(u);
 speechSynthesis.speak(new SpeechSynthesisUtterance('I am the second one!'));
 speechSynthesis.speak(new SpeechSynthesisUtterance('And I am the last one!'));
+
+window.setTimeout(function(){
+  speechSynthesis.pause();
+  window.setTimeout(function(){
+    speechSynthesis.resume();
+  }, 3000);
+}, 3000);
 
